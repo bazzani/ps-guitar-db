f8d49be1aa16f98c9ad8699dbce3bc814cb2aea4
Barry Evans <barryevans80@gmail.com>
2015-06-25 08:58:54 +0100

    Section 4 - adding queryByPriceRangeAndWoodType query to Model Jpa repo

--- src/main/java/com/guitar/db/repository/ModelJpaRepository.java
+++ src/main/java/com/guitar/db/repository/ModelJpaRepository.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.guitar.db.model.Model;
@@ -12,4 +14,9 @@ import com.guitar.db.model.Model;
 public interface ModelJpaRepository extends JpaRepository<Model, Long> {
 	List<Model> findByPriceGreaterThanEqualAndPriceLessThanEqual(BigDecimal lowest, BigDecimal highest);
 	List<Model> findByModelTypeNameIn(List<String> types);
+
+	@Query("select m from Model m where m.price >= :lowest and m.price <= :highest and m.woodType like :wood")
+	List<Model> queryByPriceRangeAndWoodType(@Param("lowest") BigDecimal lowest,
+											 @Param("highest") BigDecimal highest,
+											 @Param("wood") String wood);
 }
